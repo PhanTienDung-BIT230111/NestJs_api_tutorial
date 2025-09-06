f4383ffd1e6cf0e1de81cbce2940ca270d91d13d
PhanTienDung-BIT230111 <bit230111@st.cmcu.edu.vn>
2025-09-07 00:51:51 +0700

    updateMigrate

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
-
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService{
-  constructor(private prismaService:PrismaService){
+  constructor(private prisma:PrismaService){
 
   }
 
@@ -13,7 +13,14 @@ export class AuthService{
     return "Login successful"
   }
 
-  signup(dto: AuthDto){
-    console.log('helolo')
+  async signup(dto: AuthDto){
+    const hash = await argon.hash(dto.password)
+    const user = await this.prisma.user.create({
+      data:{
+        email:dto.email,
+        hash,
+      },
+    })
+    return user;
   }
 }
